541fd6bd39670ae46c09c117a82955e70c62751b
Aleksandr Shadrin <a_s_d@mail.ru>
2025-03-13 14:22:10 +0700

    fix: Disable Jekyll processing and update paths

--- next.config.js
+++ next.config.js
@@ -6,8 +6,8 @@ const nextConfig = {
   },
   // Настройки для GitHub Pages
   basePath: process.env.NODE_ENV === 'production' ? '/blog-post-request' : '',
-  // Используем тот же путь для статических файлов, что и для basePath
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/blog-post-request' : '',
+  // Используем тот же путь для статических файлов
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/blog-post-request/' : '',
   // Добавляем слэш в конце URL для лучшей совместимости
   trailingSlash: true,
 }
