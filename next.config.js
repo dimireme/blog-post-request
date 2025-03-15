5f28531f228d2d830bd3a22ca5ce38d7f5400ea7
Aleksandr Shadrin <a_s_d@mail.ru>
2025-03-15 08:55:39 +0700

    feat: change gh pages to vercel

--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
     unoptimized: true,
   },
-  // Если вы используете basePath для GitHub Pages
-  basePath: process.env.NODE_ENV === 'production' ? '/blog-post-request' : '',
-  // Используем тот же путь для статических файлов
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/blog-post-request/' : '',
-  // Добавляем слэш в конце URL для лучшей совместимости
-  trailingSlash: true,
 }
 
 module.exports = nextConfig 
